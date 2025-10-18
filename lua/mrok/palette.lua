2ab27471efc19110aac4fa1a9f00f0a0fa8ba2c3
Mateusz Ścianek <scianek@protonmail.com>
2025-10-18 19:03:25 +0200

    feat: define mrok palette

---
+++ lua/mrok/palette.lua
@@ -0,0 +1,28 @@
+---The color palette used by mrok
+---@class mrok.Palette
+local palette = {
+    orange = "#fec2a4",
+    green = "#6ef0c7",
+    cyan = "#69ddf5",
+    blue = "#92b2fe",
+    magenta = "#cb94ff",
+    red = "#fc7ead",
+
+    bg = "#010515",
+    bg2 = "#040d1e",
+    bg3 = "#0e182b",
+
+    gray1 = "#233459",
+    gray2 = "#4b5ea2",
+
+    fg = "#bbc6e9",
+
+    bg_orange = "#422617",
+    bg_green = "#0d362a",
+    bg_cyan = "#04343d",
+    bg_blue = "#1b2746",
+    bg_magenta = "#312040",
+    bg_red = "#3d1826",
+}
+
+return palette
